db57e0989fff269e40fad807037fc1d16b59ac25
Brandon silva <Brandon.silva091670@gmail.com>
2024-05-09 00:52:39 -0700

    Agregar archivos de proyecto.

---
+++ New_Chapter3/New_Chapter3/HeadlinesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using New_Chapter3.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace New_Chapter3
+{
+    public class HeadlinesViewModel : ViewModel
+    {
+        public HeadlinesViewModel()
+        {
+        }
+    }
+
+}
---
+++ New_Chapter3/New_Chapter3/Resolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Autofac;
+using System.Collections.Generic;
+using System.Text;
+
+namespace New_Chapter3
+{
+    public static class Resolver
+    {
+        private static IContainer container;
+        public static void Initialize(IContainer container)
+        {
+            Resolver.container = container;
+        }
+        public static T Resolve<T>()
+        {
+            return container.Resolve<T>();
+        }
+
+    }
+}
